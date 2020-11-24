766ee26b802cb318f002075ffc0b71c50e6432d4
oEduardoAfonso <54921791+oEduardoAfonso@users.noreply.github.com>
2020-11-23 21:01:51 -0300

    Adicionado .vhd de Medição de Velocidade
    No arquivo consta apenas as portas para criar o port map do componente na principal

---
+++ ProjetoFinalPED.srcs/sources_1/new/medeVelocidade.vhd
@@ -0,0 +1,16 @@
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity medeVelocidade is
+    Port ( clk3 : in STD_LOGIC;
+           sensor : in STD_LOGIC;
+           speed : out STD_LOGIC_VECTOR(11 downto 0));
+end medeVelocidade;
+
+architecture Behavioral of medeVelocidade is
+
+begin
+
+
+end Behavioral;
