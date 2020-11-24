22bdf69380915335c40425f7cc427d080fb410bb
victorhugo21 <54643372+victorhugo21@users.noreply.github.com>
2020-11-24 19:47:37 -0300

    Update indicadorDeVelocidade.vhd

--- ProjetoFinalPED.srcs/sources_1/new/indicadorDeVelocidade.vhd
+++ ProjetoFinalPED.srcs/sources_1/new/indicadorDeVelocidade.vhd
@@ -1,13 +1,13 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity indicaVelocidade is
+entity indicadorDeVelocidade is
     Port ( clk2 : in STD_LOGIC;
            speed : in STD_LOGIC_VECTOR(11 downto 0);
            led : out STD_LOGIC_VECTOR(15 downto 0));
-end indicaVelocidade;
+end indicadorDeVelocidade;
 
-architecture Behavioral of indicaVelocidade is
+architecture Behavioral of indicadorDeVelocidade is
 begin
 
 end Behavioral;
