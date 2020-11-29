08315b89eaa7cf1493888b1d83dd590801832192
thiagohdaqw <thiagohdaqw@gmail.com>
2020-11-29 11:22:43 -0300

    Implementando pwm

--- ProjetoFinalPED.srcs/sources_1/new/pwm.vhd
+++ ProjetoFinalPED.srcs/sources_1/new/pwm.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.all;
 
 entity pwm is
  Port (clk      : in STD_LOGIC;
@@ -8,8 +9,33 @@ entity pwm is
 end pwm;
 
 architecture Behavioral of pwm is
-
+    constant DIV    : Integer := 100;
+    signal cu_s     : integer range 0 to 100 := TO_INTEGER(unsigned(cu));
+    signal contador : Integer range 0 to DIV := 0;
+    signal load     : std_logic;
 begin
+    process(clk)
+        variable count : Integer range 0 to DIV := 0;
+    begin
+        if(rising_edge(clk)) then
+            count := count + 1;
+            if(count = DIV) then
+                load <= '1';
+                count := 0;
+            else
+                load <= '0';
+            end if;
+            contador <= count;
+        end if;
+    end process;
 
+    process(load)
+    begin
+        if(rising_edge(load)) then
+            cu_s <= TO_INTEGER(unsigned(cu));
+        end if;
+    end process;
 
+    pwm_out <= '1' WHEN contador < cu_s else '0';
+    
 end Behavioral;
