1d8b81f97abdf8ad2fc4275261d595228727eb20
RcleydsonR <74625814+RcleydsonR@users.noreply.github.com>
2020-12-09 15:44:06 -0300

    Update mede_velocidade.vhd

--- ProjetoFinalPED.srcs/sources_1/new/mede_velocidade.vhd
+++ ProjetoFinalPED.srcs/sources_1/new/mede_velocidade.vhd
@@ -11,17 +11,19 @@ end mede_velocidade;
 architecture Behavioral of mede_velocidade is
 signal contador : INTEGER:= 0;
 signal registrador : INTEGER:= 0;
-
+signal input : std_logic := sensor or clk3;
 begin
 
-contagem: process(sensor, clk3)
+
+contagem: process(input)
 begin
-    if(rising_edge(sensor)) then
-        contador <= contador + 1;
-    end if;
-    if(rising_edge(clk3)) then
+    if(rising_edge(input)) then
+    if(clk3 = '1') then
         registrador <= contador*60/8;
         contador <= 0;
+    elsif(sensor = '1') then
+        contador <= contador + 1;
+    end if;
     end if;
 end process contagem;
 
