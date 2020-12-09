a073f2520b3eb32f11d89551a309a4e179e90ec4
thiagohdaqw <thiagohdaqw@gmail.com>
2020-12-09 13:41:24 -0300

    melhorando pwm

--- ProjetoFinalPED.srcs/sources_1/new/pwm.vhd
+++ ProjetoFinalPED.srcs/sources_1/new/pwm.vhd
@@ -3,25 +3,25 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity pwm is
- Port (clk      : in STD_LOGIC;
+ Port (clk1      : in STD_LOGIC;
        cu       : in STD_LOGIC_VECTOR(6 downto 0);
        pwm_out  : out STD_LOGIC);
 end pwm;
 
 architecture Behavioral of pwm is
-    constant DIV    : Integer := 100;
-    signal cu_s     : integer range 0 to 100 := TO_INTEGER(unsigned(cu));
-    signal contador : Integer range 0 to DIV := 0;
+    constant DIV    : unsigned (6 downto 0):= "1100100";
+    signal cu_s     : unsigned (6 downto 0) := unsigned(cu);
+    signal contador : unsigned(6 downto 0);
     signal load     : std_logic;
 begin
-    process(clk)
-        variable count : Integer range 0 to DIV := 0;
+    process(clk1)
+        variable count : unsigned(6 downto 0) := "0000000";
     begin
-        if(rising_edge(clk)) then
+        if(rising_edge(clk1)) then
             count := count + 1;
             if(count = DIV) then
+                count := "0000000";
                 load <= '1';
-                count := 0;
             else
                 load <= '0';
             end if;
@@ -32,10 +32,16 @@ begin
     process(load)
     begin
         if(rising_edge(load)) then
-            cu_s <= TO_INTEGER(unsigned(cu));
+            cu_s <= unsigned(cu);
         end if;
     end process;
 
-    pwm_out <= '1' WHEN contador < cu_s else '0';
-    
+    process(contador, cu_s)
+    begin
+        if(contador < cu_s) then
+            pwm_out <= '1';
+        else 
+            pwm_out <= '0';
+        end if;
+    end process;
 end Behavioral;
